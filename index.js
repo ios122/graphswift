ba6bfcfa1f537b3b57994126ad3914ba355a1550
许志锋 <xuzhifeng@ling.ai>
2018-08-14 16:43:10 +0800

    feat: 增加属性字段生成逻辑.

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const fs = require('fs-plus')
 const loadJsonFile = require('load-json-file')
 const changeCase = require('change-case')
 
+/* 属性定义时,必须参考和遵循 https://realm.io/docs/swift/latest 的 Property cheatsheet 部分. */
+
+// 目前,真正需要的,有以下几种类型: --- 此处在模拟 "枚举".
+const propertyKinds = {
+  BOOL: {
+    NON_OPTIONAL: "bool-non-optional",
+    OPTIONAL: "bool-optional",
+  },
+  INT: {
+    NON_OPTIONAL: "int-non-optional",
+    OPTIONAL: "int-optional",
+  },
+  FLOAT: {
+    NON_OPTIONAL: "float-non-optional",
+    OPTIONAL: "float-optional",
+  },
+  STRING: {
+    NON_OPTIONAL: "string-non-optional",
+    OPTIONAL: "string-optional",
+  },
+  OBJECT: {
+    OPTIONAL: "object-optional",
+  },
+  LIST: {
+    NON_OPTIONAL: "list-non-optional",
+  },
+  LINKINGOBJECTS: { // TODO: 需要尽快支持,通过配置文件来指定 LINKINGOBJECTS 属性,否则可能会导致无法分离 Realm 对象.
+    NON_OPTIONAL: "linkingobjects-non-optional",
+  }
+}
+
 // 解析必须的参数.
 program
   .version(pjson.version)
@@ -68,12 +99,10 @@ function makeSwiftObjectClass(item){
 `
 //
 //  ${item.name}.swift
-//  Luka
 //
 //  注意: 该文件,自动从 Graphql 的 Schema 中生成,请勿直接修改此文件.
 //
-//  Created by Luka研发 on ${currentDate()}.
-//  Copyright © 2017年 北京物灵智能科技有限公司. All rights reserved.
+//  Created by graphswift on ${currentDate()}.
 //
 
 import Foundation
@@ -83,8 +112,8 @@ import ObjectMapper_Realm
 
 class ${item.name}: Object, Mappable {
     // MARK: 属性字段.
-    ${item.fields.reduce((result, filed)=>{
-      return result + makeSwiftProperty(filed)
+    ${item.fields.reduce((result, field)=>{
+      return result + makeSwiftProperty(field)
     }, '')}
     ${makeMappingInfo(item.fields)}
 
@@ -110,15 +139,315 @@ return rtn
 * @return   对应形式的 Swift 形式的 Property.
 */
 function makeSwiftProperty(field) {
-// TODO: 不同 kind,需要不同的模板
+  let propertyInfo = whichKindProperty(field)
 
-  let rtn = 
+  switch (propertyInfo.kind){
+    case propertyKinds.BOOL.NON_OPTIONAL: {
+      let rtn = 
 `
-    /// ${field.description}
-    @objc dynamic var ${field.name}: ${field.type.name} = ""
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: ${propertyInfo.type} = false
 `
+      return rtn
+    }
 
-  return rtn
+    case propertyKinds.BOOL.OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    let ${propertyInfo.name} = RealmOptional<${propertyInfo.type}>()
+`
+      return rtn
+    }
+
+    case propertyKinds.INT.NON_OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: ${propertyInfo.type} = 0
+`
+      return rtn
+    }
+
+    case propertyKinds.INT.OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    let ${propertyInfo.name} = RealmOptional<${propertyInfo.type}>()
+`
+      return rtn
+    }
+
+    case propertyKinds.FLOAT.NON_OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: ${propertyInfo.type} = 0.0
+`
+      return rtn
+    }
+
+    case propertyKinds.FLOAT.OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    let ${propertyInfo.name} = RealmOptional<${propertyInfo.type}>()
+`
+      return rtn
+    }
+
+    case propertyKinds.STRING.NON_OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name} = ""
+`
+      return rtn
+    }
+
+    case propertyKinds.STRING.OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: String? = nil
+`
+      return rtn
+    }
+
+    case propertyKinds.OBJECT.OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: ${propertyInfo.type}? = 0.0
+`
+      return rtn
+    }
+
+    case propertyKinds.LIST.NON_OPTIONAL: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    let ${propertyInfo.name} = List<${propertyInfo.type}>()
+`
+      return rtn
+    }
+
+    default: {
+      let rtn = 
+`
+    /// ${propertyInfo.description}
+    @objc dynamic var ${propertyInfo.name}: ${propertyInfo.type}? = nil
+`
+    return rtn
+    }
+  }
+}
+
+/**
+ * 获取 graphql 字段,对应的 swift 属性类型.
+ * @param field   graphql 中某个 type 的字段信息. 
+ * 
+ * @return  完整形式是:{name: "owner", kind: propertyKinds.OBJECT.OPTIONAL, type: "User", description:"管理员"}
+ */
+function whichKindProperty(field){
+  let name = field.name
+  let description = field.description
+
+  // 获取 List 中元素的类型.
+  let elementTypeOfList = (typeInfo)=>{
+     // "声明",不可为空的类型.
+     switch (typeInfo.kind){
+      case 'SCALAR': {
+        switch (typeInfo.name){
+          case 'Boolean': {
+            const type = 'Bool'
+            return type
+          }
+
+          case 'Int': {
+            const type = 'Int'
+            return type
+          }
+            
+          case 'Float': {
+            const type = 'Float'
+            return type
+          }
+          default: {
+            const type = 'String'
+            return type
+          }
+        }
+      }
+      default: { // 说明是一个 Object 类型.
+        const type = typeInfo.name
+        return type
+      }
+    }
+  }
+
+  if(field.type.kind === 'NON_NULL'){ // "声明",不可为空的类型.
+    const currentLevelTypeInfo = field.type.ofType
+    switch (currentLevelTypeInfo.kind){
+      case 'SCALAR': {
+        switch (currentLevelTypeInfo.name){
+          case 'Boolean': {
+            const type = 'Bool'
+            const kind = propertyKinds.BOOL.NON_OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+
+          case 'Int': {
+            const type = 'Int'
+            const kind = propertyKinds.INT.NON_OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+            
+          case 'Float': {
+            const type = 'Float'
+            const kind = propertyKinds.FLOAT.NON_OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+          default: {
+            const type = 'String'
+            const kind = propertyKinds.STRING.NON_OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+        }
+      }
+
+      case 'LIST': {
+        const type = elementTypeOfList(currentLevelTypeInfo.ofType)
+
+        const kind = propertyKinds.LIST.NON_OPTIONAL
+        
+        return {
+          name,
+          type,
+          kind,
+          description,
+        }
+      }
+
+      default: {
+        const type = currentLevelTypeInfo.name
+
+        const kind = propertyKinds.OBJECT.OPTIONAL
+        
+        return {
+          name,
+          type,
+          kind,
+          description,
+        }
+      }
+    }
+  } else { // "声明", 可为空的类型.
+    const currentLevelTypeInfo = field.type
+
+    switch (currentLevelTypeInfo.kind){
+      case 'SCALAR': {
+        switch (currentLevelTypeInfo.name){
+          case 'Boolean': {
+            const type = 'Bool'
+            const kind = propertyKinds.BOOL.OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+
+          case 'Int': {
+            const type = 'Int'
+            const kind = propertyKinds.INT.OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+            
+          case 'Float': {
+            const type = 'Float'
+            const kind = propertyKinds.FLOAT.OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+          default: {
+            const type = 'String'
+            const kind = propertyKinds.STRING.OPTIONAL
+            
+            return {
+              name,
+              type,
+              kind,
+              description,
+            }
+          }
+        }
+      }
+      
+      case 'LIST': {
+        const type = elementTypeOfList(currentLevelTypeInfo.ofType)
+
+        const kind = propertyKinds.LIST.NON_OPTIONAL
+        
+        return {
+          name,
+          type,
+          kind,
+          description,
+        }
+      }
+
+      default: {
+        const type = currentLevelTypeInfo.name
+
+        const kind = propertyKinds.OBJECT.OPTIONAL
+        
+        return {
+          name,
+          type,
+          kind,
+          description,
+        }
+      }
+    }
+  }
 }
 
 /**
@@ -136,8 +465,9 @@ function currentDate(){
  * @param fields graphql 的 type 的完整字段信息.
  */
 function makeMappingInfo(fields) {
-  let rtn = 
-`   // MARK: - Mappable
+   let rtn = 
+`   
+    // MARK: - Mappable
         
     public func mapping(map: Map) {
         ${fields.reduce((result, field)=>{
@@ -153,7 +483,6 @@ return rtn
 
 // TODO: 临时测试.
 let testItem = types[0]
-console.log(testItem.name)
 let x = makeSwiftObjectClass(testItem)
 console.log(x)
 // console.log(testItem.fields)

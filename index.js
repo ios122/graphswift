7e50a30f0eafdf0d19ffa8725e468c11f956883b
许志锋 <xuzhifeng@ling.ai>
2018-08-14 11:54:57 +0800

    feat: 确定 API 结构.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World~")
+#!/usr/bin/env node
+
+let program = require('commander')
+ 
+
+program
+  .version('0.1.0')  // TODO: 自动解析 module.json 中的版本
+  .option('-i, --input <path>', 'schema 源文件 所在目录')
+  .option('-o, --output <path>', 'swift 文件 输出目录')
+  .parse(process.argv)
+
+
+  console.log(`输入目录: ${program.input}`)
+  console.log(`输出目录: ${program.output}`)
